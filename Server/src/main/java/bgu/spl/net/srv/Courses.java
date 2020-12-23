885f4eb66c14d1aa8b281997b7121ed1a450afd1
Quit3Simpl3 <abramovichavishag@gmail.com>
2020-12-23 10:47:10 +0200

    Courses resource manager

--- Server/src/main/java/bgu/spl/net/srv/Courses.java
+++ Server/src/main/java/bgu/spl/net/srv/Courses.java
@@ -2,11 +2,14 @@ package bgu.spl.net.srv;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Courses {
     // private fields:
+    ConcurrentHashMap<Integer, Integer> courseIdToIndex;
     CopyOnWriteArrayList<Course> courses; // Using List instead of HashMap so that the order is kept.
 
     private final static class SingletonHolder {
@@ -21,13 +24,45 @@ public class Courses {
         this.courses = new CopyOnWriteArrayList<>();
     }
 
+    private void addCourse(Course course) {
+        this.courses.add(course); // Adds the course to the courses list
+        this.courseIdToIndex.put(course.getCourseNumber(), this.courses.indexOf(course));
+    }
+
     public void createCourse(int courseNumber, int numOfMaxStudents, String name, int[] kdam) {
         Course course = new Course(courseNumber, numOfMaxStudents, name, kdam);
-        this.courses.add(course); // Adds the course to the courses list
+        this.addCourse(course); // Add the course to the list and hashmap
     }
 
     public boolean register(Course course, User user) {
         return course.register(user);
+    }
+
+    /**
+    * Gets a Course object by its ID by pulling its location in the courses array from the HashMap.
+    * @param id - The course's ID number.
+     */
+    public Course getCourse(int id) {
+        try {
+            Integer index = this.courseIdToIndex.get(id);
+            if (Objects.isNull(index)) // Course not found in HashMap // TODO: test this!!!
+                for (Course course : this.courses)
+                    if (course.getCourseNumber() == id) return course;
+
+            return this.courses.get(index);
+        }
+        catch (NoSuchElementException e) {
+            throw new NoSuchElementException("Course number " + id + " does not exist.");
+        }
+    }
+
+    /**
+     * Validates all the courses' fields.
+     */
+    public boolean validateCourses() {
+        for (Course course : this.courses)
+            if (!course.isValid()) return false;
+        return true;
     }
 }
 
--- Server/src/main/java/bgu/spl/net/srv/Database.java
+++ Server/src/main/java/bgu/spl/net/srv/Database.java
@@ -21,7 +21,6 @@ import static java.lang.Integer.parseInt;
 public class Database {
 	// private fields:
 	ConcurrentHashMap<String, User> users; // users<String username, User user>
-//	ConcurrentHashMap<Integer, Course> courses; // courses<int courseNumber, Course course>
 	Courses courses;
 
 	String input_file_path;
@@ -30,10 +29,8 @@ public class Database {
 		private final static Database instance = new Database();
 	}
 
-	private Course getCourseByID(int id) {
-		for(Course course : this.courses)
-			if (course.getCourseNumber() == id) return course;
-		throw new NoSuchElementException("Course number " + id + " does not exist.");
+	public Course getCourse(int id) {
+		return this.courses.getCourse(id);
 	}
 
 	private String _get_input_file_path() {
@@ -47,9 +44,10 @@ public class Database {
 			while (reader.hasNextLine()) {
 				//File format: number|name|[kdamnumber1, kdamnumber2]|seats
 				String line = reader.nextLine();
-				String[] data = line.split("|");
+				String[] data = line.split("\\|");
+
 				// Create an array of kdam courses:
-				String[] kdam = data[2].split("|");
+				String[] kdam = data[2].split("\\|");
 				int[] kdam_numbers = new int[kdam.length];
 				for (int i = 0; i < kdam.length; i++) kdam_numbers[i] = parseInt(kdam[i]);
 
@@ -80,12 +78,6 @@ public class Database {
 	public static Database getInstance() {
 		return SingletonHolder.instance;
 	}
-
-	private boolean validateCourses() {
-		for (Course course : this.courses)
-			if (!course.isValid()) return false;
-		return true;
-	}
 	
 	/**
 	 * loades the courses from the file path specified 
@@ -93,6 +85,6 @@ public class Database {
 	 */
 	boolean initialize(String coursesFilePath) {
 		generateCoursesFromFile(coursesFilePath);
-		return validateCourses();
+		return this.courses.validateCourses();
 	}
 }
