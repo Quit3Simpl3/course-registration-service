80fea6b836701c57ba84b790c570943cf52f0e4e
assafrbi <73109803+assafrbi@users.noreply.github.com>
2020-12-31 13:42:54 +0200

    Update userMessage.cpp

--- Client/src/userMessage.cpp
+++ Client/src/userMessage.cpp
@@ -3,7 +3,6 @@
 #include "../include/userMessage.h"
 #include <boost//algorithm//string.hpp>
 
-
 using namespace std;
 
 void adminReg(char a[],std::vector<string>,ConnectionHandler* h,bool* l);
@@ -18,7 +17,6 @@ void isRegistered(char a[],std::vector<string>,ConnectionHandler* h,bool* l);
 void unRegister(char a[],std::vector<string>,ConnectionHandler* h,bool* l);
 void myCourses(char a[],std::vector<string>,ConnectionHandler* h,bool* l);
 
-
 userMessage::userMessage(ConnectionHandler *h,bool* t,bool* l) : handler(h), terminate(t),logOut(l) {
     my_map = std::map<std::string, void(*) (char a[],std::vector<string>, ConnectionHandler* h,bool* l)>();
     my_map["ADMINREG"] = adminReg;
@@ -32,10 +30,7 @@ userMessage::userMessage(ConnectionHandler *h,bool* t,bool* l) : handler(h), ter
     my_map["ISREGISTERED"] = isRegistered;
     my_map["UNREGISTER"] = unRegister;
     my_map["MYCOURSES"] = myCourses;
-
-
 }
-
 
 void userMessage::run() {
     while (!(*terminate)) {
@@ -53,11 +48,45 @@ void userMessage::run() {
     }
 }
 
-short bytesToShort(char* bytesAr) {
+short bytesToShort1(char* bytesAr) {
     short result = (short)((bytesAr[0] & 0xff)<<8);
     result += (short)(bytesAr[1] & 0xff);
     return result;
 }
+
+void sendShort(char a[], char shortMsg[], ConnectionHandler* h) {
+    char *charToSend = new char[4];
+    charToSend[0] = a[0];
+    charToSend[1] = a[1];
+    charToSend[2] = shortMsg[0];
+    charToSend[3] = shortMsg[1];
+    h->sendBytes(charToSend, 4);
+
+
+    // TODO
+    cout << "sent msg: START:";
+
+    char *w1 = new char[2];
+    w1[0] = charToSend[0];
+    w1[1] = charToSend[1];
+    char *w2 = new char[2];
+    w2[0] = charToSend[2];
+    w2[1] = charToSend[3];
+
+
+    short w01 = bytesToShort1(w1);
+    short w02 = bytesToShort1(w2);
+
+    cout << "the opcode is:" << w01 << ", ";
+    cout << "the message is:" << w02 << ". ";
+    cout << "END." << endl;
+
+    delete[] charToSend;
+
+
+
+}
+
 void stringToBytes(char a[], string line, int size, ConnectionHandler* h) {
 
     int line_size = size + 2;
@@ -83,8 +112,8 @@ void stringToBytes(char a[], string line, int size, ConnectionHandler* h) {
     w2[3] = lineToSend[3];
 
 
-    short w01 = bytesToShort(w1);
-    short w02 = bytesToShort(w2);
+    short w01 = bytesToShort1(w1);
+    short w02 = bytesToShort1(w2);
 
     cout << "the opcode is:" << w01 << ",";
     cout << "the msg is:" << w02 << ",";
@@ -95,73 +124,76 @@ void stringToBytes(char a[], string line, int size, ConnectionHandler* h) {
 
 }
 
-
-
 void shortToBytes(char* bytesAr, short num) {
     bytesAr[0] = ((num >> 8) & 0xFF);
     bytesAr[1] = (num & 0xFF);
 }
+
 void adminReg(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
     shortToBytes(a, 1);
     string send = v[1]+'\0'+v[2]+'\0';
     int len = send.length();
     stringToBytes(a,send,len,h);
-};
-
+}
 
 void studentReg(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
     shortToBytes(a, 2);
     string send = v[1]+'\0'+v[2]+'\0';
     int len = send.length();
     stringToBytes(a,send,len,h);
-};
+}
+
 void login(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
     shortToBytes(a, 3);
     string send = v[1]+'\0'+v[2]+'\0';
     int len = send.length();
     stringToBytes(a,send,len,h);
-};
+}
 
-void logout(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
+void logout(char a[],std::vector<string> v, ConnectionHandler* h, bool* l) {
     *l = true;
     shortToBytes(a, 4);
     h->sendBytes(a, 2);
 }
 
-void courseReg(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
+void courseReg(char a[], std::vector<string> v, ConnectionHandler* h, bool* l) {
     shortToBytes(a, 5);
-    string send = v[1];
-    char *course = new char[2];
-    int courseNum = std:: stoi(send);
-    short num = courseNum;
-    shortToBytes(course, courseNum);
-    int len = send.length();
-    stringToBytes(a,course,len,h);
+    short send_short = stoi(v[1]);
+    char *msg = new char[2];
+    shortToBytes(msg, send_short);
+    cout << "send_short = " << send_short << endl;
+    sendShort(a, msg, h);
 }
 
-void kdamCheck(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
+void kdamCheck(char a[],std::vector<string> v, ConnectionHandler* h, bool* l) {
     shortToBytes(a, 6);
     string send = v[1];
     int len = send.length();
     stringToBytes(a,send,len,h);
 }
 
-void courseStat(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
-    shortToBytes(a, 7);
+void courseStat(char a[],std::vector<string> v, ConnectionHandler* h, bool* l) {
+    /*shortToBytes(a, 7);
     string send = v[1];
     int len = send.length();
-    stringToBytes(a,send,len,h);
+    stringToBytes(a,send,len,h);*/
 
+    shortToBytes(a, 7);
+    short send_short = stoi(v[1]);
+    char *msg = new char[2];
+    shortToBytes(msg, send_short);
+    cout << "send_short = " << send_short << endl;
+    sendShort(a, msg, h);
 }
 
-void studentStat(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
+void studentStat(char a[],std::vector<string> v, ConnectionHandler* h, bool* l) {
     shortToBytes(a, 8);
     string send = v[1]+'\0';
     int len = send.length();
     stringToBytes(a,send,len,h);
 }
 
-void isRegistered(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
+void isRegistered(char a[],std::vector<string> v, ConnectionHandler* h, bool* l) {
     shortToBytes(a, 9);
     string send = v[1];
     int len = send.length();
@@ -178,4 +210,3 @@ void myCourses(char a[],std::vector<string> v,ConnectionHandler* h,bool* l) {
     shortToBytes(a, 11);
     h->sendBytes(a, 2);
 }
-
