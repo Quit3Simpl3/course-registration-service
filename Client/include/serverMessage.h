959074cc8c413524f0eba06badfe0a6ec27c5b5a
Quit3Simpl3 <21604833+Quit3Simpl3@users.noreply.github.com>
2021-01-06 13:50:51 +0200

    Add files via upload

--- Client/include/serverMessage.h
+++ Client/include/serverMessage.h
@@ -1,28 +1,19 @@
 #include "connectionHandler.h"
 
-
-#ifndef SPL_03_SERVERMESSAGE_H
-#define SPL_03_SERVERMESSAGE_H
-
 using namespace std;
 
 class serverMessage {
-
     public:
-
-         serverMessage(ConnectionHandler* handler,bool* terminate, bool* logOut);
-
-         void run();
+        serverMessage(ConnectionHandler* handler, bool* terminate, bool* logOut);
+        virtual ~serverMessage(); // Destructor
+        void run();
 
     private:
-    ConnectionHandler* handler;
-    bool* terminate;
-    bool* logOut;
-    std::map<int, void (*)(char a[],ConnectionHandler* h, bool* terminate, bool* logOut)> my_map;
-
+        ConnectionHandler& handler;
+        bool& terminate;
+        bool& logOut;
+        std::map<int, void (*)(char a[], ConnectionHandler& h, bool& terminate, bool& logOut)> my_map;
+        static void ack(char a[], ConnectionHandler& h, bool& terminate, bool& l);
+        static void error(char a[], ConnectionHandler& h, bool& terminate, bool& l);
+        static short bytesToShort(char* bytesAr);
 };
-
-
-
-
-#endif //SPL_03_SERVERMESSAGE_H
--- Client/include/userMessage.h
+++ Client/include/userMessage.h
@@ -1,50 +1,32 @@
 #include "connectionHandler.h"
 
-#ifndef SPL_03_USERMASSAGE_H
-#define SPL_03_USERMASSAGE_H
-
-
 using namespace std;
 
 class userMessage {
-
     public:
-
-        userMessage(ConnectionHandler* handler,bool* terminate, bool* logOut);
-
+        userMessage(ConnectionHandler* handler, bool* terminate, bool* logOut);
+        virtual ~userMessage(); // Destructor
+        userMessage(const userMessage& other); // Move-constructor
+        userMessage &operator=(const userMessage &other);
         void run();
-
-
-    //void shortToBytes(short num, char *bytesAr);
-
+        void close();
 
     private:
-        ConnectionHandler* handler;
-        bool* terminate;
-        bool* logOut;
-        std::map<std::string, void (*)(char a[],std::vector<string>,ConnectionHandler* h, bool* logOut)> my_map;
+        ConnectionHandler& handler;
+        bool& terminate;
+        bool& logOut;
+        char* msg;
+        std::map<std::string, void (userMessage::*)(char a[], std::vector<string>, ConnectionHandler& h, bool& logOut)> my_map;
 
-
-
-
-
-
-
+        void adminReg(char a[],std::vector<string>, ConnectionHandler& h, bool& l);
+        void studentReg(char a[],std::vector<string>, ConnectionHandler& h, bool& l);
+        void login(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void logout(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void courseReg(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void kdamCheck(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void courseStat(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void studentStat(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void isRegistered(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void unRegister(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
+        void myCourses(char a[],std::vector<string>,ConnectionHandler& h, bool& l);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif //SPL_03_USERMASSAGE_H
